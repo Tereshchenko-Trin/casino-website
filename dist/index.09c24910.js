fe8c17500474b41895e19070bab0092950840481
Ekaterina Tereshchenko <tereshchenko.trin@gmail.com>
2024-06-04 17:28:58 +0300

    grid section and slider section

---
+++ dist/index.09c24910.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    navigation: {
+        nextEl: ".packages__arrow-next",
+        prevEl: ".packages__arrow-back"
+    },
+    slidesPerView: 1
+});
+
+//# sourceMappingURL=index.09c24910.js.map
